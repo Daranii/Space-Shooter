cfab301154e842c3df0ffe3b601bfafc08792c74
Darani <>
2020-10-15 01:47:05 +0300

    Added InputManager class
    Currently very unfinished. It is only bare bones of what is needed for the game.

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,59 @@
+﻿using Controls;
+using Entities;
+using Helper;
+using UnityEngine;
+
+namespace Managers
+{
+    public class InputManager : BehaviourSingleton<InputManager>
+    {
+        [SerializeField] private Player player;
+        
+        private Inputs _inputs;
+        private float _horizontalDeadzone = ControlValues.DefaultHorizontalDeadzone;
+        private float _verticalDeadzone = ControlValues.DefaultVerticalDeadzone;
+
+        protected override void Init()
+        {
+            _inputs = new Inputs();
+        }
+
+        private void OnEnable()
+        {
+            _inputs.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputs.Disable();
+        }
+
+        // private void Start()
+        // {
+        //     PlayerInputSetup();
+        //     UiInputSetup();
+        // }
+
+        private void Update()
+        {
+            var movement = _inputs.Primary.Movement.ReadValue<Vector2>();
+            
+            if (movement.x > _horizontalDeadzone || movement.x < -_horizontalDeadzone 
+               || movement.y > _verticalDeadzone || movement.y < -_verticalDeadzone) 
+                player.Move(movement);
+            
+            if (_inputs.Primary.Fire.ReadValue<float>() > 0.5)
+                player.Fire();
+        }
+
+        private void PlayerInputSetup()
+        {
+            
+        }
+
+        private void UiInputSetup()
+        {
+            
+        }
+    }
+}
