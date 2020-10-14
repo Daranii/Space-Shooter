242947cfff538e3b3674819fd83401284ef133ee
Darani <>
2020-10-15 01:44:12 +0300

    Added ProjectileBehaviour and ShieldEffect classes

---
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using Helper;
+using Managers;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    [SerializeField] private float projectileSpeed = 8f;
+
+    private void Update()
+    {
+        MoveUp();
+        DespawnOffScreen();
+    }
+    
+    private void OnDisable()
+    {
+        if (transform.parent.CompareTag("PowerUpEffect"))
+            GameObjectPoolManager.Instance.ReturnObject(transform.parent.gameObject);
+        else
+            GameObjectPoolManager.Instance.ReturnObject(gameObject);
+    }
+
+    private void DespawnOffScreen()
+    { 
+        if (transform.position.y > ScreenValues.TopLimit + ScreenValues.OffScreenEffectsLength)
+        {
+            if (transform.parent.CompareTag("PowerUpEffect"))
+                transform.parent.gameObject.SetActive(false);
+            else
+                gameObject.SetActive(false);
+        }
+    }
+    
+    private void MoveUp()
+    {
+        transform.Translate(Vector3.up * (Time.deltaTime * projectileSpeed));
+    }
+}
---
+++ Assets/Scripts/ShieldEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ShieldEffect : MonoBehaviour
+{
+    private float _timeToLive;
+    
+    public void SetTimeToLive(float timeToLive) => _timeToLive = timeToLive;
+
+    public void ExtendTimeToLive(float timeToLive) => _timeToLive += timeToLive;
+
+    private void Update()
+    {
+        _timeToLive -= Time.deltaTime;
+        
+        if (_timeToLive < 0.0001f)
+            Destroy(gameObject);
+    }
+}
